5d6a0fe0bfa8c2185330a565fc92f4ff904af64c
CarlosAMoran <Carlosamoran18@gmail.com>
2021-07-23 12:12:21 -0400

    My brain finally woke up and I remembered .length for checking empty arrays

--- index.js
+++ index.js
@@ -26,7 +26,13 @@ const animals = [
  * getTotalCount(animals); //> 28
  * getTotalCount([]); //> 0 // returns 0 if the input array is empty
  */
-function getTotalCount(animals) {}
+function getTotalCount(animals) {
+  let animalcount = 0;
+  for (i = 0; i < animals.length; i++) {
+    animalcount += animals[i].count;
+  }
+  return animalcount;
+}
 
 /**
  * FUNCTION DESCRIPTION
@@ -40,7 +46,13 @@ function getTotalCount(animals) {}
  * getAllKinds(animals); //> ["Pig", "Cow", "Chicken", "Horse", "Dog", "Cat"]
  * getAllKinds([]); //> [] // returns empty array if input array is empty
  */
-function getAllKinds(animals) {}
+function getAllKinds(animals) {
+  let animalStrArr = [];
+  for (i = 0; i < animals.length; i++) {
+    animalStrArr.push(animals[i].kind);
+  }
+  return animalStrArr;
+}
 
 /**
  * FUNCTION DESCRIPTION
@@ -59,7 +71,17 @@ function getAllKinds(animals) {}
   ];
  * filterByCountMinimum([], 3); //> [] // returns empty array if input array is empty
  */
-function filterByCountMinimum(animals, minimum) {}
+function filterByCountMinimum(animals, minimum) {
+  let minCount = [];
+  for (i = 0; i < animals.length; i++) {
+    if (animals[i].count >= minimum) {
+      minCount.push(animals[i]);
+    } else {
+      continue;
+    }
+  }
+  return minCount;
+}
 
 /**
  * FUNCTION DESCRIPTION
@@ -73,7 +95,26 @@ function filterByCountMinimum(animals, minimum) {}
  * getMostCommonAnimal(animals); //> { kind: "Chicken", count: 11 }
  * getMostCommonAnimal([]); //> null // returns null if the input is empty
  */
-function getMostCommonAnimal(animals) {}
+function getMostCommonAnimal(animals) {
+  finalArr = [null];
+  if (animals.length == 0) {
+    return finalArr[0];
+  } else {
+    highestCount = [animals[0].count];
+    for (i = 0; i < animals.length; i++) {
+      let animCount = animals[i].count;
+      let anim = animals[i];
+      if (animCount > highestCount[0]) {
+        highestCount.splice(0, 1, animCount);
+        finalArr.splice(0, 1, anim);
+        console.log(finalArr);
+      } else {
+        continue;
+      }
+    }
+  }
+  return finalArr[0];
+}
 
 // Do not change anything below this line.
 module.exports = {
